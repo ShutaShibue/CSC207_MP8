b76157176307b2d22fc831502c8da61358dcc9f5
poko <shuta.shibue@gmail.com>
2023-11-16 20:32:19 -0600

    Implemented BitTree Helper Classes

---
+++ src/BitTreeClasses.java
@@ -0,0 +1,54 @@
+interface BitTreeNode {
+    public boolean isLeaf();
+} // BitTreeNode
+
+
+class BitTreeInteriorNode implements BitTreeNode{
+    BitTreeNode left;
+    BitTreeNode right;
+
+    // +--------------+------------------------------------------------
+    // | Constructors |
+    // +--------------+
+    public BitTreeInteriorNode(){
+        this.left = null;
+        this.right = null;
+    }
+
+    public boolean isLeaf() {
+        return false;
+    } // isLeaf()
+
+    public BitTreeNode left(){
+        return left;
+    }
+
+    public BitTreeNode right(){
+        return right;
+    }
+
+} // class BitTreeInteriorNode
+
+
+class BitTreeLeaf implements BitTreeNode {
+    String bits;
+    String value;
+
+    public boolean isLeaf() {
+        return true;
+    } // isLeaf()
+
+    public String bits(){
+        return bits;
+    }
+
+    public String value(){
+        return value;
+    }
+
+    public BitTreeLeaf(String bits, String value){
+        this.bits = bits;
+        this.value = value;
+    }
+
+} // class BitTreeLea
