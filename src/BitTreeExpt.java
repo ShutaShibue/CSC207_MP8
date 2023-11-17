b6211d2b0095469929110924484f856cf849b6c2
poko <shuta.shibue@gmail.com>
2023-11-16 22:45:20 -0600

    removed test file

--- src/BitTreeExpt.java
+++
@@ -1,8 +0,0 @@
-import java.io.PrintWriter;
-
-public class BitTreeExpt {
-    public static void main(String[] args) {
-        PrintWriter pen = new PrintWriter(System.out, true);
-        
-    }
-}
